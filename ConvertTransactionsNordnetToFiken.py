5ae3e343e04607e35de29435d770b094ca14afda
Jon Petter Åsen <jonpetter@sqhead.com>
2020-04-30 08:49:20 +0200

    Updated converter script.

--- ConvertTransactionsNordnetToFiken.py
+++ ConvertTransactionsNordnetToFiken.py
@@ -6,10 +6,11 @@ Created on Wed Jan 29 21:43:04 2020
 @author: jonpetter
 """
 
+#%%
 import pandas as pd
 
 #%%
-df = pd.read_csv('/Users/jonpetter/Downloads/transactions_export_2020-01-29T21_42_03.csv', 
+df = pd.read_csv('/Users/jonpetter/Downloads/transactions_export_2020-04-29T22_30_34.csv',
             sep='\t',
             encoding='utf-16',
             header=0,
@@ -19,6 +20,7 @@ df = pd.read_csv('/Users/jonpetter/Downloads/transactions_export_2020-01-29T21_4
             na_values=[])
 
 print(df)
+print([c for c in df.columns])
 
 #%%
 newDfColumns = [u"Dato", 
@@ -32,7 +34,7 @@ newDfColumns = [u"Dato",
                 u"Valuta"]
                 
 extractDataFnList = [lambda row: row[u"Bokføringsdag"],
-                     lambda row: row[u"Transaksjonstype"] + u" " + row[u"Verdipapir"],
+                     lambda row: row[u"Transaksjonstype"] + u" " + row[u"Verdipapir"] + u" " + str(row[u"Antall"]) + u", " + str(row[u"Kurs"]) + u", " + str(row[u"Avgifter"]) ,
                      lambda row: row[u"Transaksjonstype"],
                      lambda row: row[u"Verdipapir"],
                      lambda row: row[u"Beløb"] if row[u"Beløb"] > 0 else "",
@@ -50,10 +52,15 @@ def createNewLine(row):
                 
 newData = []
 for i, row in df.iterrows():
-    newData.append( createNewLine(row) )
+    if row[u"Beløb"] == 0.0:
+        print('Skipping row %s'%(row[u"Bokføringsdag"] + u" " + row[u"Transaksjonstype"] + u" " + row[u"Verdipapir"]))
+        continue
+    newData.insert(0, createNewLine(row))
     
 newDf = pd.DataFrame(newData, columns=newDfColumns)
 print(newDf)
+
+newDf.to_csv('/Users/jonpetter/Downloads/nordnet_transactions.cvs')
                 
 #newDf = df[[u'Bokføringsdag',
 #            u'Transaksjonstype',
